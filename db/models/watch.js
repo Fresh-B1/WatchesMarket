2536fe6d2df46d4b6b7327623717792e9e29d622
Fresh-B1 <ggarzumanov@gmail.com>
2024-02-29 20:00:02 +0300

    update dev branch

--- db/models/watch.js
+++ db/models/watch.js
@@ -3,11 +3,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Watch extends Model {
     static associate({ Order }) {
-<<<<<<< HEAD
-      this.hasMany(Order, { foreignKey: 'watchID' });
-=======
       this.hasMany(Order, { foreignKey: 'watchId' });
->>>>>>> 3b6d8cd600b4e34515be15f3bdd3c7bd502aa398
     }
   }
   Watch.init(
@@ -28,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Watch',
-    }
+    },
   );
   return Watch;
 };
