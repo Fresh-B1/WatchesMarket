8738136f2308837fb7169aa127f45dcdcb690e30
Fresh-B1 <ggarzumanov@gmail.com>
2024-02-29 13:03:56 +0300

    добавлены кнопки в navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,27 +5,38 @@ function Navbar({ user }) {
     <nav>
       <div>
         <ul>
-          <li>
-            <a href='/'>Main</a>
+          <li className='logo'>
+            <img src="./img/logo.png" alt="Logo" />
           </li>
-          <li>
-            <a href='/books'>Watches</a>
+          <li className="nav_center">
+            О нас
+            {/* <a href='/books'>Watches</a> */}
+          </li>
+          <li className="nav_center">
+            Популярное
+          </li>
+          <li className="nav_center">
+            Продукция
+          </li>
+          <li className="nav_center">
+            Заказать
+          </li>
+          <li className="nav_center">
+            Контакты
           </li>
           {user ? (
             <>
               <li>
                 <a>{user.name}</a>
               </li>
-              <li className='nav_right'>
-                <a href='/api/auth/logout'>Logout</a>
+              <li>
+                <a href="/api/auth/logout">Logout</a>
               </li>
             </>
           ) : (
-            <>
-              <li>
-                <a href='/auth'>Sign-in</a>
-              </li>
-            </>
+            <li>
+              {/* <a href='/auth'>Sign-in</a> */}
+            </li>
           )}
         </ul>
       </div>
