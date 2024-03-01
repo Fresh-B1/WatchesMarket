b16f80e3de40a5de5258688e5364dc2d185c5113
Fresh-B1 <ggarzumanov@gmail.com>
2024-03-01 12:28:55 +0300

    update styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,33 +5,33 @@ function Navbar({ user }) {
     <nav>
       <div>
         <ul>
-          <li className='logo'>
-            <img src='./img/logo.png' alt='Logo' />
+          <li className="logo">
+            <img src="./img/logo.png" alt="Logo" />
           </li>
-          <a href='#aboutUs'>
-            <li className='nav_center'>
+          <a href="#aboutUs">
+            <li className="nav_center">
               О нас
               {/* <a href='/books'>Watches</a> */}
             </li>
           </a>
-          <li className='nav_center'>Популярное</li>
-          <a href='#watchBlock'>
-            <li className='nav_center'>Продукция</li>
+          <a href="#carouselExampleDark"><li className="nav_center">Популярное</li></a>
+          <a href="#watchBlock">
+            <li className="nav_center">Продукция</li>
           </a>
-          <a href='#'>
-            <li className='nav_center'>Заказать</li>
+          <a href="#">
+            <li className="nav_center">Заказать</li>
           </a>
-          <a href='#footer'>
-            <li className='nav_center'>Контакты</li>
+          <a href="#footer">
+            <li className="nav_center">Контакты</li>
           </a>
           {user ? (
             <>
-              <li className='nav_center'>Режим админа</li>
-              <a href='/orders'>
-                <li className='nav_center'>Заказы</li>
+              <li className="nav_center">Режим админа</li>
+              <a href="/orders">
+                <li className="nav_center">Заказы</li>
               </a>
-              <a href='/api/auth/logout'>
-                <li className='nav_center'>Logout</li>
+              <a href="/api/auth/logout">
+                <li className="nav_center">Logout</li>
               </a>
             </>
           ) : null}
