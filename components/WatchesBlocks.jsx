83b6be0321071b991d336fd77d68e90091e87798
Fresh-B1 <ggarzumanov@gmail.com>
2024-03-01 09:26:40 +0300

    corrected main page

--- components/WatchesBlocks.jsx
+++ components/WatchesBlocks.jsx
@@ -4,27 +4,27 @@ function WatchesBlocks() {
   return (
     <div id="watchBlock" className="container_for_watches">
       <div className="watch_1">
-        <div className="watch_1_block_left"><p className="watch_title title_1">SPLASH DANCE</p> <p className='watch_description description_1'>Текстурированный циферблат синего цвета металлик с зеленым принтом и светящимися элементами символизирует динамичный поток и мощь водопада. Эти ультратонкие прочные часы из нержавеющей стали мгновенно привнесут нотку горпкор-гламура в любой образ. Пришло время продемонстрировать всем ритмы вашего стиля!</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
+        <div className="watch_1_block_left"><p className="watch_title title_1">ALLURING OAK</p> <p className='watch_description description_1'>Текстурированный циферблат из дуба символизирует спокойствие и величие природы. Эти ультратонкие прочные часы из дерева мгновенно привнесут нотку горпкор-гламура в любой образ. Пришло время продемонстрировать всем ритмы вашего стиля!</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
         <div className="watch_1_block_right"><img src="./img/watch/watch_1.png" alt="watch #1" /></div>
       </div>
 
       <div className="watch_2">
         <div className="watch_2_block_left"><img src="./img/watch/watch_13.png" alt="watch #2" /></div>
-        <div className="watch_2_block_right"><p className="watch_title title_2">SUNBAKED SANDSTONE</p> <p className='watch_description description_2'>Бежевый циферблат с зернистой текстурой и ремешок из структурированного текстиля этих потрясающих часов из нержавеющей стали напоминают волнистые узоры песчаных дюн и яркое солнце пустыни. Часы призваны олицетворять сияние пустыни и непреходящую изящность природы.</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
+        <div className="watch_2_block_right"><p className="watch_title title_2">DARK WOOD</p> <p className='watch_description description_2'>Черный циферблат с гладкой текстурой и ремешок из темного дерева. Часы призваны олицетворять спокойствие и современный стиль одухотворенный природой.</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
       </div>
 
       <div className="watch_3">
-        <div className="watch_3_block_left"><p className="watch_title title_3">FROST BLOOM</p> <p className='watch_description description_3'>Эти эффектные часы BIG BOLD с текстурированным циферблатом синего цвета металлик и 3D-индексами большого размера в прочном корпусе из нержавеющей стали с пескоструйной обработкой навевают ощущение ледяного холода горных вершин. Они придадут вашему образу ледяную элегантность, а вам — чувство драйва. </p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
+        <div className="watch_3_block_left"><p className="watch_title title_3">GOLDEN WOOD</p> <p className='watch_description description_3'>Эти эффектные женские часы с гладким черным циферблатом и яркими вставками из дерева шлефованной обработки. Они придадут вашему образу элегантность. </p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
         <div className="watch_3_block_right"><img src="./img/watch/watch_10.png" alt="watch #3" /></div>
       </div>
 
       <div className="watch_4">
         <div className="watch_4_block_left"><img src="./img/watch/watch_11.png" alt="watch #4" /></div>
-        <div className="watch_4_block_right"><p className="watch_title title_4">BY THE BONFIRE</p> <p className='watch_description description_4'>Текстурированный черный циферблат, а также землистые тона этих часов с хронографом призваны напомнить об успокаивающей силе леса и тепле лесного костра, а также внести приключенческую ноту в вашу повседневность. Идеальный аксессуар для посиделок у костра под звездами или для жизни в городе.</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
+        <div className="watch_4_block_right"><p className="watch_title title_4">RARE BREGUETTE STYLE</p> <p className='watch_description description_4'>Классические брегеты секстурированным деревянным циферблатом, призваны напомнить об успокаивающей силе леса и тепле лесного костра. Идеальный аксессуар, чтобы подчеркнуть свой тонкий вкус в ритме современного города.</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
       </div>
 
       <div className="watch_5">
-        <div className="watch_5_block_left"><p className="watch_title title_5">SPLASH DANCE</p> <p className='watch_description description_5'>Текстурированный циферблат синего цвета металлик с зеленым принтом и светящимися элементами символизирует динамичный поток и мощь водопада. Эти ультратонкие прочные часы из нержавеющей стали мгновенно привнесут нотку горпкор-гламура в любой образ. Пришло время продемонстрировать всем ритмы вашего стиля!</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
+        <div className="watch_5_block_left"><p className="watch_title title_5">BLACK SAPPHIRE</p> <p className='watch_description description_5'>Текстурированный циферблат черного цвета с метллическими вставками. Эти прочные часы мгновенно привнесут нотку горпкор-гламура в любой образ.</p><a className='btn_wb_buy' href="">ЗАКАЗАТЬ</a></div>
         <div className="watch_5_block_right"><img src="./img/watch/watch_5.png" alt="watch #5" /></div>
       </div>
 
